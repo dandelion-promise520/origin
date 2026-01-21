86237dd700377725ea5278543d6f76c876c3a367
dandelion <1993466159@qq.com>
2026-01-21 10:59:40 +0800

    feat(src): 重构Menu侧边栏组件完成

--- src/renderer/src/components/Menu/index.tsx
+++ src/renderer/src/components/Menu/index.tsx
@@ -1,53 +1,42 @@
-import { Flex } from '@radix-ui/themes'
 import React from 'react'
 
 import { NavLink } from 'react-router'
 
 const Menu: React.FC = () => {
   return (
-    <Flex justify="between" align="center" direction="column" height="100%">
-      <Flex
-        direction="column"
-        justify="center"
-        align="center"
-        gap="4"
-        id="menu"
-        className="no-drag"
-      >
-        <Flex
-          direction="column"
-          justify="center"
-          align="center"
-          gap="4"
-          className="after:h-0.5 after:w-8 after:rounded-xl after:bg-gray-300 after:content-['']"
-        >
-          <NavLink to="/">
+    <div className="flex h-full flex-col items-center justify-between">
+      <header className="center flex-col gap-4" id="menu">
+        <section className="center flex-col gap-4">
+          <NavLink to="/" className={'no-drag'}>
             <span className="icon-[fa7-solid--home] size-6" />
           </NavLink>
-        </Flex>
+        </section>
 
-        <Flex direction="column" gap="4">
-          <NavLink to="schedule">
+        {/* 分割线 */}
+        <div className="w-8 rounded-full border-2 border-gray-300"></div>
+
+        <section className="center flex-col gap-4">
+          <NavLink to="schedule" className={'no-drag'}>
             <span className="icon-[mingcute--schedule-fill] size-7" />
           </NavLink>
-          <NavLink to="members">
+          <NavLink to="members" className={'no-drag'}>
             <span className="icon-[tdesign--member-filled] size-6" />
           </NavLink>
-          <NavLink to="dashboard">
+          <NavLink to="dashboard" className={'no-drag'}>
             <span className="icon-[streamline-ultimate--presentation-board-graph-bold] size-6" />
           </NavLink>
-        </Flex>
-      </Flex>
+        </section>
+      </header>
 
-      <section
+      <main
         style={{ writingMode: 'vertical-rl' }}
         className="text-xl font-bold text-black select-none"
       >
         rentabel
-      </section>
+      </main>
 
-      <section className="icon-[tabler--message] no-drag size-10 cursor-pointer" />
-    </Flex>
+      <footer className="icon-[tabler--message] no-drag size-10 cursor-pointer" />
+    </div>
   )
 }
 
